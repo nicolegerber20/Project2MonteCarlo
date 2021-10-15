8603c3fc4cb7a4773cb873b9156bf686d7efeec4
Nicole Gerber <nicolegerber20@gmail.com>
2021-10-15 15:07:32 -0400

    MultiThreadMonte

--- SingleThreadMonteCarlo/src/SingleThreadMonteCarlo.java
+++ SingleThreadMonteCarlo/src/SingleThreadMonteCarlo.java
@@ -1,13 +1,14 @@
+package SingleThreadMonteCarlo;
+
 import java.time.Duration;
-        import java.time.Instant;
-        import java.util.concurrent.*;
+import java.time.Instant;
+import java.util.concurrent.*;
 
 public class SingleThreadMonteCarlo {
-
     public static void main(String[] args) {
         long total_points = 200_000;
 
-        for (int i=0; i<3; i++) {
+        for (int i = 0; i < 3; i++) {
             Instant startTime = Instant.now();
 
             double pi = simulation(total_points);
@@ -18,25 +19,22 @@ public class SingleThreadMonteCarlo {
 
             System.out.println("The calculated pi value is: " + pi);
             System.out.println("Number of points used: " + total_points);
-            System.out.println("Time taken: "+timeElapsed+" milliseconds.\n");
-            total_points = total_points*100;
+            System.out.println("Time taken: " + timeElapsed + " milliseconds.\n");
+            total_points = total_points * 100;
         }
     }
 
-    private static double simulation(long total_points){
+    private static double simulation(long total_points) {
         long withinCircle = 0;
 
-        for(int i=0; i<total_points; i++){
+        for (int i = 0; i < total_points; i++) {
             double x = ThreadLocalRandom.current().nextDouble(-1, 1);
             double y = ThreadLocalRandom.current().nextDouble(-1, 1);
-            if (x*x + y*y <= 1){
+            if (x * x + y * y <= 1) {
                 withinCircle++;
             }
         }
-
-        double pi = withinCircle / (double)total_points * 4;
+        double pi = withinCircle / (double) total_points * 4;
         return pi;
-
     }
-
 }
